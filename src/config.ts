89c23015c7991f531627249bb79e5c89e10d8511
hector fonseca <hectormago666@gmail.com>
2024-03-04 19:55:29 -0300

    cambio2

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Hector Fonseca",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "Bienes Raices Laura Esteve",
+  title: "Laura Esteve Bienes Raices",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 4,
